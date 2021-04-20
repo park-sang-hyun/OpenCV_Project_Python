5aab74dbe460420f956afb9b8fe31a16bb8c6262
Sanghyun Park <park03851@naver.com>
2021-04-20 23:05:30 +0900

    [Add] magnification and reduction using matrices

---
+++ 05.geometric_transform/scale_matrix.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('../img/fish.jpg')
+height, width = img.shape[:2]
+
+# 0.5배 축소 변환행렬
+m_small = np.float32([[0.5, 0, 0], [0, 0.5, 0]])
+
+# 2배 확대 변환행렬
+m_big = np.float32([[2, 0, 0], [0, 2, 0]])
+
+# 보간법 적용 없이 확대/축소
+dst1 = cv2.warpAffine(img, m_small, (int(height * 0.5), int(width * 0.5)))
+dst2 = cv2.warpAffine(img, m_big, (int(height * 2), int(width * 2)))
+
+# 보간법 적용한 확대/축소
+dst3 = cv2.warpAffine(img, m_small, (int(height * 0.5), int(width * 0.5)), None, cv2.INTER_AREA)
+dst4 = cv2.warpAffine(img, m_big, (int(height * 2), int(width * 2)), None, cv2.INTER_CUBIC)
+
+# 결과 출력
+cv2.imshow('original', img)
+cv2.imshow('small', dst1)
+cv2.imshow('big', dst2)
+cv2.imshow('small INTER_AREA', dst3)
+cv2.imshow('big INTER_CUBIC', dst4)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
